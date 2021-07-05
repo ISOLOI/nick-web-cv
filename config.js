d692d4084802b86ce76e32b686bdd4158f7f14b9
ISOLOI <nicholasjkaufmann@gmail.com>
2021-07-05 02:48:06 -0400

    updated config

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#537a60',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/nick-web-cv/`, // This path is subpath of your hosting https://domain/portfolio
   firstName: 'Nicholas',
   lastName: 'Kaufmann',
   // social
